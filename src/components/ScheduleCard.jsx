ad48921697ad790d6cd54985d2ad43286e4e6905
= <=>
2025-09-10 12:53:27 -0400

    took out the spots left on schedule card so it doesnt show how many are left

--- src/components/ScheduleCard.jsx
+++ src/components/ScheduleCard.jsx
@@ -1,46 +1,61 @@
 "use client";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { format, parseISO } from "date-fns";
 import CheckoutButton from "./CheckoutButton";
 import { formatInTimeZone } from "date-fns-tz";
 
-export default function ScheduleCard({ classInfo,bookedClasses,setBookedClasses }) {
+export default function ScheduleCard({
+  classInfo,
+  bookedClasses,
+  setBookedClasses,
+}) {
   const [showDescription, setShowDescription] = useState(false);
-  const isClassBooked = Array.isArray(bookedClasses) && bookedClasses.some((c) => {
-    const cid = c?.fields?.['Class ID'];
-    const classIdMatch = Array.isArray(cid) ? String(cid[0]) === String(classInfo.id) : false;
-    const bookingIdMatch = String(c.id) === String(classInfo.id);
-    return classIdMatch || bookingIdMatch;
-  });
-  
-  
-
+  const isClassBooked =
+    Array.isArray(bookedClasses) &&
+    bookedClasses.some((c) => {
+      const cid = c?.fields?.["Class ID"];
+      const classIdMatch = Array.isArray(cid)
+        ? String(cid[0]) === String(classInfo.id)
+        : false;
+      const bookingIdMatch = String(c.id) === String(classInfo.id);
+      return classIdMatch || bookingIdMatch;
+    });
 
   return (
-    <div key={classInfo.id} className="bg-black shadow rounded-lg p-2 mb-2 flex flex-col gap-1 w-full max-w-[170px] mx-auto sm:max-w-[180px]">
+    <div
+      key={classInfo.id}
+      className="bg-black shadow rounded-lg p-2 mb-2 flex flex-col gap-1 w-full max-w-[170px] mx-auto sm:max-w-[180px]"
+    >
       <div className="flex items-center justify-between">
         <h3 className="text-sm font-bold text-[#C5a572] truncate">
           {classInfo["Class Name"]}
         </h3>
+      </div>
+      {/* taken out for now since it shows how many spots are left */}
+      {/* <div>
         <span className="text-xs text-white">
           {classInfo["Available Spots"] > 0
             ? `${classInfo["Available Spots"]} left`
             : "Full"}
         </span>
-      </div>
+      </div> */}
       <div className="flex items-center justify-between text-xs text-white">
-        <span>{formatInTimeZone(
+        <span>
+          {formatInTimeZone(
             classInfo["Class Time"],
-            'America/New_York',
+            "America/New_York",
             "h:mm a"
-          )}</span>
+          )}
+        </span>
         <span>-</span>
-        <span>{formatInTimeZone(
+        <span>
+          {formatInTimeZone(
             classInfo["End Time"],
 
-            'America/New_York',
+            "America/New_York",
             "h:mm a"
-          )}</span>
+          )}
+        </span>
       </div>
       <button
         className="text-xs text-red-700 font-bold underline text-left"
@@ -65,7 +80,9 @@ export default function ScheduleCard({ classInfo,bookedClasses,setBookedClasses 
         metaData={classInfo.id}
         isBooked={isClassBooked}
         popular={false}
-        onBooked={(classInfo)=> setBookedClasses( [...bookedClasses,classInfo])}
+        onBooked={(classInfo) =>
+          setBookedClasses([...bookedClasses, classInfo])
+        }
       />
     </div>
   );
